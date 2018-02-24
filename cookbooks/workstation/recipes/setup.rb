961aad5c9542b719457b97e5a01357536771a36f
Carlos E Z Batistao <cezbatistao@gmail.com>
2018-02-24 10:38:15 -0300

    Using node objects to get node information

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -4,8 +4,21 @@ end
 
 package 'ntp'
 
+package 'git' do
+  action :install
+end
+
+# aqui esta sendo acessado as informacoes do ohai, via node objects, ex: ohai memory (linha de comando)
+ipaddress = node['ipaddress']
+total_memory = node['memory']['total']
+
 file '/etc/motd' do
-  content 'This is the property of Carlos E Z Batistao'
+  content "This is the property of Carlos E Z Batistao
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{ipaddress}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{total_memory}
+  "
   mode '0770'
   owner 'ceb'
   group 'root'
