4c0b74cd993d723b3c4eab8bb9a683958ba69baf
Carlos E Z Batistao <cezbatistao@gmail.com>
2018-02-24 11:52:21 -0300

    Using variables with template resources

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -8,8 +8,17 @@ package 'git' do
   action :install
 end
 
+# aqui esta sendo acessado as informacoes do ohai, via node objects, ex: ohai memory (linha de comando)
+ipaddress = node['ipaddress']
+total_memory = node['memory']['total']
+
 template '/etc/motd' do
   source 'motd.erb'
+  variables(
+    :name => 'Carlos E Z Batistao',
+    :ipaddress => ipaddress,
+    :total_memory => total_memory
+  )
   mode '0770'
   owner 'ceb'
   group 'root'
