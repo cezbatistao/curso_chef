ab30a75f5f70a728d19f9ec8b8000cfc7cc2ddbb
Carlos E Z Batistao <cezbatistao@gmail.com>
2018-02-24 10:46:17 -0300

    Using node object to print metadata machine info in page and update cookbook version

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -7,7 +7,10 @@
 package 'apache2' # apache2 para ubuntu, ou httpd para redhat, deixei especifico msm para ubuntu
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello world Chef Apache Server</h1>'
+  content "<h1>Hello world Chef Apache Server</h1>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>IPADDRESS: #{node['ipaddress']}</h2>
+  "
 end
 
 service 'apache2' do
