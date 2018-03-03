68aeac71cd719d90717c8c2ce44e67dff9a6d0a9
Carlos E Z Batistao <cezbatistao@gmail.com>
2018-03-03 11:36:24 -0300

    Include static file with cookbook_file and download resource with remove_file

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -6,6 +6,14 @@
 
 package 'apache2' # apache2 para ubuntu, ou httpd para redhat, deixei especifico msm para ubuntu
 
+cookbook_file '/var/www/html/static_index.html' do
+  source 'static_index.html'
+end
+
+remote_file '/var/www/html/hulk_img.jpg' do
+  source 'http://nerdgeekfeelings.com/wp-content/uploads/2015/08/hulk.jpg'
+end
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
 end
